bc8e197133ade0d609f44714b07b58aada341532
OxySokolova <poksanaa16@gmail.com>
2024-02-18 16:38:24 -0600

    Filter change

--- src/Components/Filter/AllCategories.js
+++ src/Components/Filter/AllCategories.js
@@ -5,8 +5,7 @@ const AllCategories = () => {
     return (
     <div>
         <h1>What kind of food do you like?</h1>
-        {['SEAFOOD', 'ITALIAN', 'APPETIZERS', 'SALADS', 'ALL'].
-        map((category, index )=> <Filter key={index} category = {category}/>)}
+        {['SEAFOOD', 'ITALIAN', 'APPETIZERS', 'SALADS', 'ALL'].map((category, index )=> <Filter key={index} category = {category}/>)}
     </div>)
 }
 export default AllCategories;
